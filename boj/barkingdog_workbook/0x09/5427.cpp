6e98bf4c7eb6b2a8cdb98eb78269dee35c795578
i-chaeyeon <leechae0605@naver.com>
2025-03-08 00:29:57 +0900

    250307 BFS 연습문제

--- boj/barkingdog_workbook/0x09/5427.cpp
+++ boj/barkingdog_workbook/0x09/5427.cpp
@@ -18,11 +18,11 @@ int main(void){
     cin >> T;
 
     for(int i=0; i<T; i++){
-        //board, fire, vis 0으로 초기화
+        //board, fire, vis 초기화
         for(int j=0; j<1001; j++){
             fill(board[j], board[j]+1000, 0);
-            fill(fire[j], fire[j]+1000, 0);
-            fill(vis[j], vis[j]+1000, 0);
+            fill(fire[j], fire[j]+1000, -1);
+            fill(vis[j], vis[j]+1000, -1);
         }
 
         int w, h;
@@ -42,60 +42,49 @@ int main(void){
                 } else if (c == '@'){
                     board[j][k] = 1;
                     visQ.push({j,k});
-                    vis[j][k] = 1;
-                } else if (c == '.'){
+                    vis[j][k] = 0;
+                } else if (c == '*'){
                     board[j][k] = 1;
                     fireQ.push({j,k});
-                    fire[j][k] = 1;
+                    fire[j][k] = 0;
+                } else if (c == '.'){
+                    board[j][k] = 1;
                 }
             }
         }
+        
 
         while(!fireQ.empty()){
             auto cur = fireQ.front(); fireQ.pop();
             for(int dir=0; dir<4; dir++){
                 int nx = cur.X + dx[dir];
                 int ny = cur.Y + dy[dir];
-                if(nx < 0 || nx >= w || ny < 0 || ny >= h) continue;
-                if(board[nx][ny] == -1 || fire[nx][ny] != 0) continue;
+                if(nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+                if(board[nx][ny] == -1 || fire[nx][ny] > -1) continue;
                 fire[nx][ny] = fire[cur.X][cur.Y] + 1;
                 fireQ.push({nx, ny});
             }
         }
-
-        for(int j=0; j<h; j++){
-            for(int k=0; k<w; k++){
-                cout << fire[j][k];
-            }
-            cout << "\n";
-        }
-
-
+        int isOut = 0;
         while(!visQ.empty()){
-            int isOut = 0;
             auto cur = visQ.front(); visQ.pop();
             for(int dir=0; dir<4; dir++){
                 int nx = cur.X + dx[dir];
                 int ny = cur.Y + dy[dir];
-                if(board[nx][ny] == 0){
+                if(nx < 0 || nx >= h || ny < 0 || ny >= w) {
                     isOut = 1;
-                    cout << vis[cur.X][cur.Y] << "\n";
+                    cout << vis[cur.X][cur.Y]+1 << "\n";
                     break;
                 }
-                if(nx < 0 || nx >= w || ny < 0 || ny >= h) continue;
-                if(board[nx][ny] == -1 || vis[nx][ny] != 0 || fire[nx][ny] <= vis[nx][ny]) continue;
+                if(board[nx][ny] == -1 || vis[nx][ny] > -1 ) continue;
+                if(fire[nx][ny] != -1 && fire[nx][ny] <= vis[cur.X][cur.Y]+1) continue;
                 vis[nx][ny] = vis[cur.X][cur.Y] + 1;
                 visQ.push({nx, ny});
             }
             if(isOut == 1) break;
         }
 
-        for(int j=0; j<h; j++){
-            for(int k=0; k<w; k++){
-                cout << vis[j][k];
-            }
-            cout << "\n";
-        }
-
+        if(!isOut) 
+            cout << "IMPOSSIBLE\n";
     }
 }
